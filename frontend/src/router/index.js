625854730a98994dbc9173c9e8f86e3cee0060b9
CGinMax <cginmax1998@gmail.com>
2021-08-13 13:21:51 +0800

    update

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import MyVuetify from '@/components/MyVuetify'
 
 Vue.use(Router)
 
@@ -9,7 +10,13 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
+      component: MyVuetify
+    },
+    {
+      path: '/vue',
+      name: 'MyVuetify',
       component: HelloWorld
+
     }
   ]
 })
